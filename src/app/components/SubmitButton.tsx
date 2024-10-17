adbceb133f2a95adb4d18cc15dcaa94e2516f9d3
Beto Carlos <me@beto.codes>
2024-10-17 14:50:45 -0600

    Adds form logic using a formAction (#2)

--- src/app/components/SubmitButton.tsx
+++ src/app/components/SubmitButton.tsx
@@ -1,12 +1,24 @@
 import type { ComponentProps, PropsWithChildren } from 'react'
+import { useFormStatus } from 'react-dom'
+import { Loader } from 'lucide-react'
 
 export default function SubmitButton({ className, children, ...props }: PropsWithChildren<ComponentProps<'button'>>) {
+	const { pending } = useFormStatus()
+
 	return (
 		<button
 			{...props}
 			className={`flex w-40 items-center justify-center rounded-md bg-slate-800 px-4 py-3 text-white transition hover:bg-slate-900 aria-[disabled=true]:bg-slate-700 ${className}`}
+			aria-disabled={pending}
 		>
-			{children}
+			{pending ? (
+				<Loader
+					className="animate-spin"
+					size={20}
+				/>
+			) : (
+				children
+			)}
 		</button>
 	)
 }
