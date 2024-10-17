2a869a14a20d8f26e9b3df9c7d8e338b9edca054
Beto Carlos <me@beto.codes>
2024-10-17 15:37:08 -0600

    Adds button to copy url

--- src/app/components/Messages.tsx
+++ src/app/components/Messages.tsx
@@ -1,5 +1,6 @@
 import type { CreateShortUrlResponse } from '@/schemas'
-import { OctagonX } from 'lucide-react'
+import { OctagonX, Copy, Check } from 'lucide-react'
+import { useRef, useState } from 'react'
 
 interface Props {
 	state: CreateShortUrlResponse
@@ -12,10 +13,44 @@ export default function Messages({ state }: Props) {
 		</div>
 	)
 
+	const [copiedIsVisible, setCopiedIsVisible] = useState(false)
+	const ref = useRef<HTMLParagraphElement>(null)
+	const handleCopy = async () => {
+		const text = ref.current?.innerText
+		if (text) {
+			await navigator.clipboard.writeText(text).then(
+				() => {
+					setCopiedIsVisible(true)
+				},
+				(e) => {
+					console.log(e)
+					console.log('Unable to copy')
+				}
+			)
+		}
+	}
+
 	const successMessage = (
 		<div className="flex flex-col items-start gap-1">
 			<span className="text-xs">Shorty:</span>
-			<p className="font-semibold">{`${window.location.href}${state.message}`}</p>
+			<div className="flex items-center justify-center gap-3">
+				<p
+					className="font-semibold"
+					ref={ref}
+				>{`${window.location.href}${state.message}`}</p>
+				<div>
+					<button
+						type="button"
+						onClick={(e) => {
+							e.stopPropagation()
+							handleCopy()
+						}}
+						className="flex h-8 w-8 items-center justify-center rounded-md border border-slate-300 bg-white"
+					>
+						{copiedIsVisible ? <Check size={16} /> : <Copy size={16} />}
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 
